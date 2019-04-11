02a6b9e6db7ed6c782ff46d031ccf4e552cf64d0
pnxenopoulos <pnxenopoulos@gmail.com>
2019-04-11 10:22:07 -0400

    added basic req and setup shells

---
+++ setup.py
@@ -0,0 +1,15 @@
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta data
+NAME = 'cav-keras'
+DESCRIPTION = 'Concept activation vectors for Keras'
+URL = ''
+EMAIL = ''
+AUTHOR = ''
+REQUIRES_PYTHON = ''
+VERSION = None
