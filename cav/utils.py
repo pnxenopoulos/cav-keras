c232030be68c897415d0aaff82b0cfb8fd22cf20
pnxenopoulos <pnxenopoulos@gmail.com>
2019-04-23 13:16:57 -0400

    added model split function, linted

--- cav/utils.py
+++ cav/utils.py
@@ -1,73 +1,27 @@
-import numpy as np
-import keras
-from keras.datasets import cifar10, cifar100
-from keras.preprocessing.image import ImageDataGenerator
+''' Utilities for concept activation vectors '''
+
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
 
-def returnModelLayer(model, layer):
-    '''
-    '''
-    model_cut = Sequential()
-    model_post = Sequential()
-    for l in range(0,layer+1):
-        model_cut.add(model.layers[l])
-    return model_pre, model_post
+def return_split_models(model, layer):
+    ''' Function to split a model into model_f and model_h
 
-def returnLayerActivations(model_cut, x):
-    '''
-    '''
-    activations = model_cut.predict(x)
-    # maybe flatten
-    return activations
+    Parameters
+    ----------
+    model : (keras.engine.sequential.Sequential)
+        Keras sequential model to split
+    layer : (int)
+        Integer specifying layer to split model on
 
-def trainLinearClassifier(activations, y_concept, opt):
+    Returns
+    -------
+    model_f : (keras.engine.sequential.Sequential)
+        Keras sequential model that is the first part
+    model_h : (keras.engine.sequential.Sequential)
+        Keras sequential model that is the second part
     '''
-    '''
-    binary_linear_classifier = Sequential()
-    binary_linear_classifier.add(Dense(1, input_shape=activations.shape[1:], activation='sigmoid', use_bias=False))
-    binary_linear_classifier.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
-    binary_linear_classifier.fit(activations, y_concept, batch_size=32, epochs=10, shuffle=True, verbose=1)
-    return binary_linear_classifier
-
-def conceptSensitivity(x, binary_linear_classifier, activations, cav_vector):
-    '''
-    '''
-
-
-def prepCIFAR10(batch_size = 32, epochs = 20, optimizer = keras.optimizers.Adam(lr=0.001)):
-    '''
-    '''
-    num_classes = 10
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
-    # Start
-    model = Sequential()
-    model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
-    model.add(Activation('relu'))
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(64, (3, 3), padding='same'))
-    model.add(Activation('relu'))
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(512))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes))
-    model.add(Activation('softmax'))
-    # train the model
-    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train /= 255
-    x_test /= 255
-    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test), shuffle=True, verbose=1)
-    return x_train, y_train, model
+    model_f, model_h = Sequential(), Sequential()
+    for current_layer in range(0, layer+1):
+        model_f.add(model.layers[current_layer])
+    for current_layer in range(layer+1, len(model.layers)):
+        model_h.add(model.layers[current_layer])
+    return [model_f, model_h]
--- tests/tests.py
+++ tests/tests.py
@@ -4,3 +4,6 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from cav import *
+
+if __name__ == '__main__':
+    unittest.main()
