728781c7c75845c0482dcf860e0b217a2266692f
pnxenopoulos <pnxenopoulos@gmail.com>
2019-04-23 13:28:25 -0400

    added and linted binary classifier function

--- cav/utils.py
+++ cav/utils.py
@@ -1,9 +1,10 @@
 ''' Utilities for concept activation vectors '''
 
 from keras.models import Sequential
+from keras.layers import Dense
 
 def return_split_models(model, layer):
-    ''' Function to split a model into model_f and model_h
+    ''' Split a model into model_f and model_h
 
     Parameters
     ----------
@@ -25,3 +26,27 @@ def return_split_models(model, layer):
     for current_layer in range(layer+1, len(model.layers)):
         model_h.add(model.layers[current_layer])
     return [model_f, model_h]
+
+def train_concept_classifier(model_f, x_concept, y_concept):
+    ''' Train the binary classifier for the concept
+
+    Parameters
+    ----------
+    model_f : (keras.engine.sequential.Sequential)
+        First Keras sequential model from return_split_models()
+    x_concept : (numpy.ndarray)
+        Training data for concept set, has same size as model training data
+    y_concept : (numpy.ndarray)
+        Labels for concept set, has same size as model training labels
+
+    Returns
+    -------
+    binary_classifier : (keras.engine.sequential.Sequential)
+        Binary classifier Keras model
+    '''
+    concept_activations = model_f.predict(x_concept)
+    binary_classifier = Sequential()
+    binary_classifier.add(Dense(2, input_shape=concept_activations.shape[1:], activation='sigmoid'))
+    binary_classifier.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
+    binary_classifier.fit(concept_activations, y_concept, batch_size=32, epochs=20, shuffle=True)
+    return binary_classifier
