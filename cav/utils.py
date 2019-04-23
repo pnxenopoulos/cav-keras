57aa71bbc05c69dc3909c8f7b8ee3259a5c4b6e8
pnxenopoulos <pnxenopoulos@gmail.com>
2019-04-23 13:40:16 -0400

    added/linted conceptual sensitivity function

--- cav/utils.py
+++ cav/utils.py
@@ -1,7 +1,10 @@
 ''' Utilities for concept activation vectors '''
+import numpy as np
 
+from keras import backend as k
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import Adam
 
 def return_split_models(model, layer):
     ''' Split a model into model_f and model_h
@@ -50,3 +53,29 @@ def train_concept_classifier(model_f, x_concept, y_concept):
     binary_classifier.compile(loss='binary_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
     binary_classifier.fit(concept_activations, y_concept, batch_size=32, epochs=20, shuffle=True)
     return binary_classifier
+
+def conceptual_sensitivity(example, model_f, model_h, concept_cav):
+    ''' Return the conceptual conceptual sensitivity for a given example
+
+    Parameters
+    ----------
+    example : (numpy.ndarray)
+        Example to calculate the concept sensitivity (be sure to reshape)
+    model_f : (keras.engine.sequential.Sequential)
+        First Keras sequential model from return_split_models()
+    model_h : (keras.engine.sequential.Sequential)
+        Second Keras sequential model from return_split_models()
+    concept_cav : (numpy.ndarray)
+        Numpy array with the linear concept activation vector for a given concept
+
+    Returns
+    -------
+    sensitivity : (float32)
+        Sensitivity for inputted examples
+    '''
+    model_f_activations = model_f.predict(example)[0]
+    gradients = k.gradients(model_h.output, model_h.input)
+    gradient_func = k.function([model_h.input], gradients)
+    calc_grad = gradient_func([model_f_activations])[0]
+    sensitivity = np.dot(calc_grad, concept_cav)
+    return sensitivity
