ebf744623c9b01a258f2e60b255d2537cf005309
pnxenopoulos <pnxenopoulos@gmail.com>
2019-04-30 22:32:29 -0400

    added cav function tests

--- cav/test_cav.py
+++ cav/test_cav.py
@@ -1,5 +1,8 @@
 import unittest
 
+import numpy as np
+
+from keras.initializers import Constant
 from keras.models import Sequential
 from keras.layers import Dense
 
@@ -32,6 +35,41 @@ class TestModelSplit(unittest.TestCase):
         model.add(Dense(10))
         model_f, model_h = return_split_models(model, 2)
         self.assertEqual(model_h.input_shape, (None, 30))
+
+class TestBinaryClassifier(unittest.TestCase):
+    ''' Tests for CAV function
+    '''
+    def test_dimensions(self):
+        ''' Test the dimensions of the trained CAV
+        '''
+        model = Sequential()
+        model.add(Dense(5, kernel_initializer=Constant(value=9), input_dim = 5))
+        model.add(Dense(4, kernel_initializer=Constant(value=9)))
+        model.add(Dense(3, kernel_initializer=Constant(value=9)))
+        model.add(Dense(2, kernel_initializer=Constant(value=9)))
+        model.add(Dense(1, kernel_initializer=Constant(value=9)))
+        model_f, model_h = return_split_models(model, 2)
+        x_concept = np.array([[6,7,8,9,10], [5,6,7,8,9], [1,2,3,4,5], [2,3,4,5,6]])
+        y_concept = [1, 1, 0, 0]
+        cav = train_cav(model_f, x_concept, y_concept)
+        self.assertEqual(cav.shape[0], 3)
+
+    def test_cav_output(self):
+        ''' Test the output of the trained CAV
+        '''
+        np.random.seed(1996)
+        model = Sequential()
+        model.add(Dense(5, kernel_initializer=Constant(value=9), input_dim = 5))
+        model.add(Dense(4, kernel_initializer=Constant(value=9)))
+        model.add(Dense(3, kernel_initializer=Constant(value=9)))
+        model.add(Dense(2, kernel_initializer=Constant(value=9)))
+        model.add(Dense(1, kernel_initializer=Constant(value=9)))
+        model_f, model_h = return_split_models(model, 2)
+        x_concept = np.array([[6,7,8,9,10], [5,6,7,8,9], [1,2,3,4,5], [2,3,4,5,6]])
+        y_concept = [1, 1, 0, 0]
+        cav = train_cav(model_f, x_concept, y_concept)
+        cav_rounded = np.round(cav)
+        self.assertEqual(np.sum(np.array([[-1], [0], [0]]) == cav_rounded), 3)
 
 if __name__ == '__main__':
     unittest.main()
